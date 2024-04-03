afca5657f3f4d5a3f785023fcfcc5652fabc5ffe
Jingham2510 <jspingham@gmail.com>
2024-04-03 19:54:29 +0100

    -Can add tokens to column            -Checks if column is full
    Co-authored-by: benlewisheritage <benlewisheritage@users.noreply.github.com>

--- grid.py
+++ grid.py
@@ -29,7 +29,6 @@ class grid:
     #Drops a marker into the grid
     def addMarker(self, column, markerType):
         
-
         selectedColumn = self.matrix[:, column]
 
         #Check column isnt full
@@ -40,10 +39,24 @@ class grid:
         #Place marker in lowest spot
         
         """
-        Flip column
         Find lowest empty row
         Place token in matrix (via known column and found row)
         """
+
+        #Go through the slots in the column
+        rowSlot = self.rowNum - 1
+        for slot in selectedColumn[::-1]:
+            if slot == 0:
+                break
+            #Increase the slot 
+            rowSlot = rowSlot -1 
+
+        #Place the marker in the slot
+        self.matrix[rowSlot, column] = markerType.value
+
+
+        
+
             
 
 
@@ -57,15 +70,19 @@ class grid:
     #Checks if there are four in a row
     def checkConnect(self):
 
+        
+        #Get matrix size
+        mSize = self.matrix.size
+        
         #Iterate through rows and columns, check all surrounding
-
+        
         
         #If same ENUM val, check in direction until winConnNum
         pass
 
 
 
-if __name__ == "main":
+if __name__ == "__main__":
 
  
 
@@ -73,3 +90,17 @@ if __name__ == "main":
 
 
     testGrid.printGrid()
+
+
+    player1Marker = markerType.BLUE
+    player2Marker = markerType.PINK
+
+    testGrid.addMarker(2, player1Marker)
+    testGrid.printGrid()
+
+    testGrid.addMarker(1, player2Marker)
+    testGrid.printGrid()
+    
+    testGrid.addMarker(2, player1Marker)   
+    testGrid.printGrid()
+
